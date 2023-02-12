5043c3fa30f56999886bc9caa1482b4044f1c1a0
ChristineWest <christinewwwest@gmail.com>
2023-02-12 15:04:37 -0500

    added logic to change hero image based on thumbnail clicked

--- script.js
+++ script.js
@@ -22,3 +22,41 @@ reduce.addEventListener("click", () => {
     count.innerHTML = itemCount;
   }
 });
+
+// Switch out hero images with the click of a thumbnail image
+const images = [
+  {
+    thumbnail: "./images/image-product-1-thumbnail.jpg",
+    hero: "./images/image-product-1.jpg",
+  },
+  {
+    thumbnail: "./images/image-product-2-thumbnai.jpg",
+    hero: "./images/image-product-2.jpg",
+  },
+  {
+    thumbnail: "./images/image-product-3-thumbnail.jpg",
+    hero: "./images/image-product-3.jpg",
+  },
+  {
+    thumbnail: "./images/image-product-4-thumbnail.jpg",
+    hero: "./images/image-product-4.jpg",
+  },
+];
+
+const thumbnails = document.querySelectorAll(".thumbnail");
+const heroImage = document.querySelector(".hero-image");
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    console.log("you've been clicked");
+    console.log(images[index].hero);
+    heroImage.src = images[index].hero;
+  });
+});
+// Add modal cart contents
+// create ul to hold all the purchased items
+// each list contains an image of the item, the section they are from, individual item price, quantity and total price, as well as the trash can icon that can delete this entry
+// STEP 1: create ul to hold all purchased items
+const list = document.createElement("ul");
+// STEP 2: create li to hold each purchased items
+const listItems = [];
+//add thumbnail image of the product added to cart
