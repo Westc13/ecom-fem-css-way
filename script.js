491de40c6d1d2bc0c9e1972a5bc86f7855428770
ChristineWest <christinewwwest@gmail.com>
2023-02-12 13:18:07 -0500

    built counter function

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+// Open Nav Cart Content
 const modal = document.getElementById("modal");
 
 const cart = document.getElementById("nav-cart");
 cart.addEventListener("click", () => {
   modal.classList.toggle("modal-open");
+});
+
+// Change Count of items using plus and minus buttons
+const count = document.getElementById("count");
+const add = document.getElementById("plus");
+let itemCount = 0;
+add.addEventListener("click", () => {
+  itemCount++;
+  count.innerHTML = itemCount;
+});
+
+const reduce = document.getElementById("minus");
+reduce.addEventListener("click", () => {
+  if (itemCount > 0) {
+    itemCount--;
+    count.innerHTML = itemCount;
+  }
 });
